ccf31170d3addd90c75f9df3d0ba8bd78a1d35be
Garrett Weston <51564201+gkweston@users.noreply.github.com>
2019-08-23 14:55:58 -0500

    Update Player.cs

--- Landing/Player.cs
+++ Landing/Player.cs
@@ -3,45 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof (Controller2D))]
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour    // in reality "Player" is a alias for the LM & CSM game object(s)
 {
     
-    private float _appliedGravity;
-    public float gravity = -1.6f;
+
+    public float gravity = -1.6f;    // allows gravity to be changed for testing purposes
+    private float _appliedGravity;    //calculated based on set gravity to define acceleration each frame
     
-    private float _ascentModuleMass = 2445f;
+    private float _ascentModuleMass = 2445f;    // these floats will be updated to reflect historically accurate values
     private float _massBasedThrust = 7040f;
     private float _sustainedThrust;
     public float powerThrust = 5f;
-    private bool _powerThrustMode;
     public float abortThrust = 5500f;
     
     public Vector3 velocity;
-    public Vector3 landerPosition;
+    public Vector3 landerPosition;    // this will be updated to a Transform, allows LM & CSM position to be shared between the two
     Controller2D controller;
     
-    private ParticleSystem _downwardThrust;
-    public ParticleSystem LHSParticleSystem;
+    private ParticleSystem _downwardThrust;    // only private ParticleSystem (PS) as it is defined in direct relation to game object
+    public ParticleSystem LHSParticleSystem;    // public to allow PS's to be attached to game object and update their position
     public ParticleSystem RHSParticleSystem;
     public ParticleSystem UpperParticleSystem;
-    public Camera mainCamera;
+    
+    public Camera mainCamera;    // allows camera to track game object
     private Vector3 cameraPosition;
+    
+    private bool _powerThrustMode;    // for easy definition of extra thrust (currently only two powers)
+
     
     void Start() 
     {
         controller = GetComponent<Controller2D> ();
         _downwardThrust = GetComponent<ParticleSystem>();
-        _sustainedThrust = (_massBasedThrust / (_ascentModuleMass)) * Time.deltaTime;    // sans propellant mass
+        _sustainedThrust = (_massBasedThrust / (_ascentModuleMass)) * Time.deltaTime;    // used to allow reflect of historically accurate parameters
         _appliedGravity = gravity * Time.deltaTime;
     }
 
     public void Update()
     {
         
-        Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+        Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));    // currently unused, useful for defining axis
 
         
-        var lowerEmission = _downwardThrust.emission;
+        var lowerEmission = _downwardThrust.emission;    // allow seperate game object PS's to be controlled by game object
         var LHSEmission = LHSParticleSystem.emission;
         var LHSSpeed = LHSParticleSystem.main.startSpeed;
         var RHSEmission = RHSParticleSystem.emission;
@@ -49,48 +53,47 @@ public class Player : MonoBehaviour
         var UpperEmission = UpperParticleSystem.emission;
         var UpperSpeed = UpperParticleSystem.main.startSpeed;
         
-        _downwardThrust.Stop();
+        _downwardThrust.Stop();    // defaults PS status as stopped
         LHSParticleSystem.Stop();
         RHSParticleSystem.Stop();
         UpperParticleSystem.Stop();
 
-        Vector3 landerPosition = transform.position;
+        Vector3 landerPosition = transform.position;    // transform for updating position given thrust and gravity
         
-        //  if landerPosition < 10 particle emission startlifetime = 5
-        _downwardThrust.startLifetime = landerPosition.y < 20 ? 5 : 0.4f;
-        //
+        _downwardThrust.startLifetime = landerPosition.y < 20 ? 5 : 0.4f;    // changes characteristics of PS below game object to emulate lunar dust upon landing
         
-        if (controller.collisions.below || controller.collisions.above)
+        
+        if (controller.collisions.below || controller.collisions.above)    // calls collision info to bring game object to rest if colliding w/ lunar surface
         {
             velocity.x = 0;
             velocity.y = 0;
         }
         
-        _powerThrustMode = Input.GetKey(KeyCode.Space);
+        _powerThrustMode = Input.GetKey(KeyCode.Space);    // defines the extra thrust power which alters acceleration and PS output
         
-        if (Input.GetKeyDown((KeyCode.RightShift)))    
+        if (Input.GetKeyDown((KeyCode.RightShift)))    // abort thrust which will be converted into ascent thrust when LM leaves lunar surface
         {
             velocity.y += (abortThrust + gravity) * Time.deltaTime;
             lowerEmission.rateOverTime = 10000f;
         }
         
-        if (Input.GetKey((KeyCode.UpArrow))) 
+        if (Input.GetKey((KeyCode.UpArrow)))    // (###) identical thrust and PS control process across all directions
         {
-            _downwardThrust.Play();
-            lowerEmission.rateOverTime = 500f;
+            _downwardThrust.Play();    // show PS when thrust is applied
+            lowerEmission.rateOverTime = 500f;    // define default PS intensity
             
             if (_powerThrustMode)
             {
-                velocity.y += (powerThrust + gravity) * Time.deltaTime;
+                velocity.y += (powerThrust + gravity) * Time.deltaTime;    // increase acceleration from thrust and PS intensity if power thrust is active
                 lowerEmission.rateOverTime = 1500.0f;
             }
             else
             {
-                velocity.y += _sustainedThrust + _appliedGravity;
+                velocity.y += _sustainedThrust + _appliedGravity;    // default non-power thrust values
             }
         }
 
-        if (Input.GetKey((KeyCode.DownArrow))) 
+        if (Input.GetKey((KeyCode.DownArrow)))    // refer to (###)
         {
             
             UpperParticleSystem.Play();
@@ -99,7 +102,7 @@ public class Player : MonoBehaviour
             {
                 velocity.y -= (powerThrust + gravity) * Time.deltaTime;
                 UpperEmission.rateOverTime = 1000f;
-                UpperSpeed = 150f;
+                UpperSpeed = 150f;    // tertiary PS characteristic; likely to be removed for simplicity
 
             }
             else
@@ -110,7 +113,7 @@ public class Player : MonoBehaviour
             }
         }
 
-        if (Input.GetKey((KeyCode.RightArrow))) 
+        if (Input.GetKey((KeyCode.RightArrow)))    // refer to (###)
         {
             LHSParticleSystem.Play();
             
@@ -128,7 +131,7 @@ public class Player : MonoBehaviour
             }
         }
 
-        if (Input.GetKey((KeyCode.LeftArrow))) 
+        if (Input.GetKey((KeyCode.LeftArrow)))    // refer to (###)
         {
             
             RHSParticleSystem.Play();
@@ -147,14 +150,13 @@ public class Player : MonoBehaviour
             }
         }
         
-        velocity.y += _appliedGravity;
-        controller.Move (velocity * Time.deltaTime);
+        velocity.y += _appliedGravity;    // update velocity
+        controller.Move (velocity * Time.deltaTime);    // utilize Move void defined in Controller2D to update transform
 
-        LHSParticleSystem.transform.position = landerPosition;
+        LHSParticleSystem.transform.position = landerPosition;    // ensures PSs translate as LM & CSM translate
         RHSParticleSystem.transform.position = landerPosition;
         UpperParticleSystem.transform.position = landerPosition;
         mainCamera.transform.position = landerPosition;
-        mainCamera.transform.Translate(0, 0, -5);
-        
+        mainCamera.transform.Translate(0, 0, -5);    // ensures camera tracks LM & CSM
     }
 }
